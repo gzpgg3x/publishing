a52ed23cd84457c7251831e6c150ef9c508a053f
Forrest Pan <gzpgg3x@yahoo.com>
2013-05-12 00:26:09 -0400

    availableStringExtracted

--- bookieGoer/discover/api.py
+++ bookieGoer/discover/api.py
@@ -1,3 +1,5 @@
+# import glib
+import nltk
 import sys 
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 import re
@@ -33,137 +35,105 @@ def new_shout(request):
     author = request.POST['author']
     message = request.POST['message']
     keywords = request.POST['keywords']
-    print keywords
-    # kw = keywords.replace(" ", "%20")
-    # kw = kw.replace('"', "%22")
+    kw = keywords.replace(" ", "+")
+    bkkw = keywords.replace(" ", "_")
+    bkkw = "_" + bkkw
+    # bkkw = "_" + bkkw + '" c'
     # print kw
-
     zip="10001"
     address = ""
-
     d=author
     dd=message
-    # d="10/04/2012"
-    # dd ="10/04/2012"
-
     a=0
     response = []
-    for page in range(1):
+    # ddd = 'http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text='+keywords+'&sort=Date&formType=Form8K&isAdv=true&stemming=true&numResults=100&fromDate=' + d + '&toDate=' + dd + '&numResults=100' 
+    ddd = 'http://nypl.bibliocommons.com/search?t=title&q=' + kw + '&commit=Search&searchOpt=catalogue' 
+    responser=get_url_content(ddd)
+    # responser='# encoding: utf-8' + responser
+    # print bkkw
+    # bkind = responser.find("html")
+    # bkind = responser.find(bkkw)
+    # print bkind
+    for link in BeautifulSoup(responser, parseOnlyThese=SoupStrainer('a')):
+        if link.has_key('href'):
+            y = link['href']
+            if bkkw in y:      
+                print y
+                bkcode = y[11:25]
 
-        pageStr = str(page)
-        ddd = 'http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text='+keywords+'&sort=Date&formType=Form8K&isAdv=true&stemming=true&numResults=100&fromDate=' + d + '&toDate=' + dd + '&numResults=100' 
-        #ddd = 'http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=%22item%205.02%22&sort=Date&formType=Form8K&isAdv=true&stemming=true&numResults=100&fromDate=' + d + '&toDate=' + d + '&numResults=100'
-        #ddd='http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=%22item+5.02%22&startDoc=' + pageStr + '01&numResults=50&isAdv=true&stemming=true&sort=Date&formType=Form8K&fromDate=' + d + '&toDate=' + dd
-        responser=get_url_content(ddd)
-        #responser=get_url_content('http://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=%22Item%205.02%22%20AND%20%22Departure%20of%20Directors%20or%20Certain%20Officers%3B%20Election%20of%20Directors%3B%20Appointment%20of%20Certain%20Officers%3B%20Compensatory%20Arrangements%20of%20Certain%20Officers%22&sort=Date&formType=Form8K&isAdv=true&stemming=true&numResults=100&fromDate=08/09/2012&toDate=08/09/2012&numResults=100')
-
-        #addrs=[]
-
-        #a=0
-        xLast=""
-        zz=""
-        x=" "
-        e=0
-        #response = []
-        for link in BeautifulSoup(responser, parseOnlyThese=SoupStrainer('a')):
-            if link.has_key('href'):
-                y = link['href']
-
-                if 'javascript:opennew(' in y: # and xLast is not x:
-
-                    b = y.index("'",21)
-                    z = y[20:b]
-                    #print z
-
-                    c = y.index("'", b+4)
-                    x = y[b+3:c]
-                    if not (xLast == x):# and zz is z[1:45]:
-                        #a=a+1
-                        text = get_url_content(z)
-                        #print text
-                        zzz=dehtml(text)
-                        #try:
-                        #    zzz=zzz[1:8000]
-                        #except:
-                        #    zzz=zzz
-                        #print len(zzz)
-                        xx=x[0:40]                
-                        #print xx
-                        try:
-                            e = zzz.index(kw ,1) #zzz.index("Item 5.02",1)
-                        except:
-                            e = 1
-                        #print e
-                        #    e = zzz.index("ITEM 5.02",1)
-                        newzzz = zzz[e:e+250]
-                        #print newzzz
-                        #print newzzz
-                        zz=z[1:45]
-                        xLast=x
-                        #if ("a" in newzzz):
-                        #if ("resign" in newzzz) or ("retir" in newzzz) or ("to pursu" in newzzz) or ("terminat" in newzzz) or ("transition" in newzzz) or ("depart" in newzzz) or ("severanc" in newzzz) or ("interim" in newzzz) or ("successor" in newzzz) or ("step down" in newzzz) or ("separation" in newzzz) or (" cease" in newzzz) or ("eliminat" in newzzz) or ("leav" in newzzz) or ("left" in newzzz) or ("stepped down" in newzzz) or ("stepping down" in newzzz):
-                        #a = a + 1
-                        #print a
-                        author=xx
-                        #print author
-                        message=z #newzzz
-                        #print message
-                        cikidxbeg = y.index("http://www.sec.gov/Archives/edgar/data/",1) + 40
-                        cikidxend = y.index("/", cikidxbeg)
-                        cik = y[cikidxbeg-1:cikidxend]
-                        cikText = get_url_content('http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=' + cik)
-                        mailerLocA = cikText.index("<div class=\"mailer\">",1)
-                        mailerLocB = cikText.index("<div class=\"mailer\">", mailerLocA + 20)
-                        mailerText = cikText[mailerLocA:mailerLocB + 20]
-                        #print mailerText
-                        #print(dehtml(mailerText))
-                        address = dehtml(mailerText)
-                        #print address
-                        postal_code = re.match('^.*?(\d+)$', address)
-                        #print postal_code
-                        if postal_code!=None:
-                            zip = address[-5:len(address)]
-
-                            if zip[-5:-4] == "-":
-                                zip = address[-10:-6]
-
-                            if zip >= "07000" and zip <="09000":
-                                a = a + 1
-                                #print a
-                                #zip = postal_code.group(1)
-                                #print zip
-                                #print zip[-5:-4]
-                                address = address[16:len(address)]
-                                #print address                   
-
-                                shout = Shout.objects.create(lat=lat,lng=lng,author=author,message=message)
-
-                                response.append({
-                                    'date_created': shout.date_created.strftime("%b %d at %I:%M:%S%p"),
-                                    'lat': str(shout.lat),
-                                    'lng': str(shout.lng),
-                                    'author': author,
-                                    'message': message,
-                                    'zipcode': zip,
-                                    'address': address,
-                                    'count': a
-                                })
+                bkcode = 'http://nypl.bibliocommons.com/item/show_circulation/' + bkcode
+                # print bkcode 
+                responser=get_url_content(bkcode)
+                # beg = responser.index("Available to borrow",1)
+                # end = responser.index("Available to borrow",beg)
+                # responser = responser[beg:end]
+                clean_text = nltk.clean_html(responser)
+                beg = clean_text.index("Available to borrow",1)
+                end = clean_text.index("Not available at this time",beg)
+                clean_text = clean_text[beg:end]                
+                print clean_text
+                break
 
 
 
-                        #sleep(5)
-        # response = {
-        #     'date_created': shout.date_created.strftime("%b %d at %I:%M:%S%p"),
-        #     'lat': str(shout.lat),
-        #     'lng': str(shout.lng),
-        #     'author': author,
-        #     'message': message,
-        #     'zipcode': zip
-        # }    
-        
+    xLast=""
+    zz=""
+    x=" "
+    e=0
+    for link in BeautifulSoup(responser, parseOnlyThese=SoupStrainer('a')):
+        if link.has_key('href'):
+            y = link['href']
+            if 'javascript:opennew(' in y: # and xLast is not x:
+                b = y.index("'",21)
+                z = y[20:b]
+                c = y.index("'", b+4)
+                x = y[b+3:c]
+                if not (xLast == x):# and zz is z[1:45]:
+                    text = get_url_content(z)
+                    zzz=dehtml(text)
+                    xx=x[0:40]                
+                    try:
+                        e = zzz.index(kw ,1) #zzz.index("Item 5.02",1)
+                    except:
+                        e = 1
+                    newzzz = zzz[e:e+250]
+                    zz=z[1:45]
+                    xLast=x
+                    author=xx
+                    message=z
+                    cikidxbeg = y.index("http://www.sec.gov/Archives/edgar/data/",1) + 40
+                    cikidxend = y.index("/", cikidxbeg)
+                    cik = y[cikidxbeg-1:cikidxend]
+                    cikText = get_url_content('http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=' + cik)
+                    mailerLocA = cikText.index("<div class=\"mailer\">",1)
+                    mailerLocB = cikText.index("<div class=\"mailer\">", mailerLocA + 20)
+                    mailerText = cikText[mailerLocA:mailerLocB + 20]
+                    address = dehtml(mailerText)
+                    postal_code = re.match('^.*?(\d+)$', address)
+                    if postal_code!=None:
+                        zip = address[-5:len(address)]
+
+                        if zip[-5:-4] == "-":
+                            zip = address[-10:-6]
+
+                        if zip >= "07000" and zip <="09000":
+                            a = a + 1
+                            address = address[16:len(address)]
+                            shout = Shout.objects.create(lat=lat,lng=lng,author=author,message=message)
+
+                            response.append({
+                                'date_created': shout.date_created.strftime("%b %d at %I:%M:%S%p"),
+                                'lat': str(shout.lat),
+                                'lng': str(shout.lng),
+                                'author': author,
+                                'message': message,
+                                'zipcode': zip,
+                                'address': address,
+                                'count': a
+                            })
+
     return HttpResponse(json.dumps(response))
 
-#def get_shouts(request):
 def get_shouts(request):
     lat = float(request.GET['lat'])
     lng = float(request.GET['lng'])
